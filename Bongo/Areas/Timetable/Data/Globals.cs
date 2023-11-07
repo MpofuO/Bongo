3b23a1ade1b41aecb3ad66eb6ff66301568de286
OMpofu <mpofuoliver1109@gmail.com>
2023-11-07 14:02:44 +0200

    first commit

---
+++ Bongo/Areas/Timetable/Data/Globals.cs
@@ -0,0 +1,9 @@
+﻿using Bongo.Areas.TimetableArea.Models;
+
+namespace Bongo.Areas.TimetableArea.Data
+{
+    public static class Globals
+    {
+        public static Timetable timetable { get; set; }
+    }
+}
---
+++ Bongo/Areas/Tutoring/Data/IModuleRepository.cs
@@ -0,0 +1,12 @@
+﻿using Bongo.Areas.Tutoring.Models;
+
+namespace Bongo.Areas.Tutoring.Data
+{
+    public interface IModuleRepository
+    {
+        void Add(Module module);
+        void Delete(Module module);
+        void SaveChanges();
+        IEnumerable<Module> GetAllSessions();
+    }
+}
